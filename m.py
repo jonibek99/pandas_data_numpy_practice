12b94d23e7c42db8497cb4dd0db2a2a9c37c0288
jonibek99 <jonibekannaev5@gmail.com>
2025-10-27 22:13:30 +0500

    solo

---
+++ m.py
@@ -0,0 +1,5 @@
+#from pr_1 import *
+def main():
+    import pr_1
+if __name__=='__main__':
+    main()
--- pr_1.py
+++ pr_1.py
@@ -1,26 +1,24 @@
-
 import pandas as pd
 import requests as rq
 
 users = ['jonibek_99',  'shamsiyev', 'zopirov','HUSANBOI001','Sanjar1218','Jaloliddin_n1']
 all_data = []
-
 for user in users:
     url = f"https://www.codewars.com/api/v1/users/{user}"
     response = rq.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        if "username" in data:
-            all_data.append({
-                "username": data.get("username"),
-                # "fullname": data.get("name", "No name"),
-                # "honor": data.get("honor", 0),
-                # "rank_name": data.get("ranks", {}).get("overall", {}).get("name", "Unknown"),
-                "totalCompleted": data.get("codeChallenges", {}).get("totalCompleted")})
+    try:
+        if response.status_code == 200:
+            data = response.json()
+            if "username" in data:
+                all_data.append({
+                    "username": data.get("username"),
+                    # "fullname": data.get("name", "No name"),
+                    # "honor": data.get("honor", 0),
+                    # "rank_name": data.get("ranks", {}).get("overall", {}).get("name", "Unknown"),
+                    "totalCompleted": data.get("codeChallenges", {}).get("totalCompleted")})
         else:
             print(f"⚠️ {user} topilmadi yoki API xato: {data}")
-    else:
+    except Exception as user:
         print(f"❌ {user} uchun API xato: {response.status_code}")
 
 if all_data:
